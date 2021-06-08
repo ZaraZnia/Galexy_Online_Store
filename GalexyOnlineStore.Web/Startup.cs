0f330558f678eeb4aaa12931bafacb3ff5568ae6
Zahra Zaeemnia <ZahraZa@systemgroup.net>
2021-06-08 18:07:00 +0430

    Administrator Layout

--- GalexyOnlineStore.Web/Startup.cs
+++ GalexyOnlineStore.Web/Startup.cs
@@ -51,6 +51,11 @@ namespace GalexyOnlineStore.Web
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
             });
         }
     }
